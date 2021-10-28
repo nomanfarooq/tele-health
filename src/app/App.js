95de9d99bbfccd7b9e9a328be528f39995a707ac
Noman Farooq <nomanfarooq1432@gmail.com>
2021-10-28 17:09:18 +0500

    Scss Structure done

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TeleHealth = () => {
+	return (
+		<div>
+			<h1>Let Start...</h1>
+		</div>
+	);
+};
+
+export default TeleHealth;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import TeleHealth from './app/App';
+import './sass/tele-health.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TeleHealth />
   </React.StrictMode>,
   document.getElementById('root')
 );
